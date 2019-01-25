bca010a4c02a0470d4b1c6c351869a66d3c7562b
greg171 <gregory.peters.2016@gmail.com>
2019-01-25 15:17:37 +0000

    Added some basic logic for checking the neighbours of a particular cell to game, also added a reprint of the grid once an iteration has been completed. Now its time to take this coding and make it more applicable to the BBC challenge.

--- src/org/launchcode/game.java
+++ src/org/launchcode/game.java
@@ -17,5 +17,50 @@ public class game {
             }
             System.out.println();
         }
+        this.checkNeighbors();
+    }
+
+    void checkNeighbors() {
+        int i = 0;
+        for (int row = 0; row < this.grid.length; row++) {
+            for (int col = 0; col < this.grid[row].length; col++) {
+                for (int drow = 0; drow < this.directions.length; drow++) {
+                    int x = (this.directions[drow][this.directions[drow].length - this.directions[drow].length]);
+                    int y = (this.directions[drow][this.directions[drow].length - 1]);
+                    x = row + x;
+                    y = col + y;
+                    if (x < 0) {
+                        x += this.grid.length;
+                    } else if (x + 1 > this.grid.length) {
+                        x -= x;
+                    }
+                    if (y < 0) {
+                        y += this.grid[row].length;
+                    } else if (y + 1 > this.grid[row].length) {
+                        y -= y;
+                    }
+                    if (this.grid[x][y] == "x|") i++;
+                }
+                this.neighbors[row][col] = i;
+                i = 0;
+            }
+        }
+        this.showGrid();
+    }
+    void showGrid() {
+        for (int row = 0; row < this.grid.length; row++) {
+            for (int cell = 0; cell < this.grid[row].length; cell++) {
+                if (this.grid[row][cell] == " |" && this.neighbors[row][cell] == 3) {
+                    this.grid[row][cell] = "x|";
+                } else if (this.grid[row][cell] == "x|" && this.neighbors[row][cell] > 3) {
+                    this.grid[row][cell] = " |";
+                } else if (this.grid[row][cell] == "x|" && this.neighbors[row][cell] < 2) {
+                    this.grid[row][cell] = " |";
+                }
+                System.out.print(this.grid[row][cell]);
+            }
+            System.out.println();
+        }
+        this.checkNeighbors();
     }
 }
