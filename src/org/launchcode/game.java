19ae888f30439f424640f0a59e62f58c7ab4c7ac
greg171 <gregory.peters.2016@gmail.com>
2019-01-25 15:35:41 +0000

    Fixed up some logic within the checkNeighbour method so it is more readable. Also set some identifiers to static and private and they are either not being changed or not being accesed by anything outwith the class.

--- src/org/launchcode/game.java
+++ src/org/launchcode/game.java
@@ -3,12 +3,13 @@ package org.launchcode;
 public class game {
 
 
-    String[][] grid;
-    int[][] neighbors;
-    int[][] directions = {{0, 1}, {1, 1}, {1, 0}, {1, -1}, {0, -1}, {-1, -1}, {-1, 0}, {-1, 1}};
+    private String[][] grid;
+    private int[][] neighborsCounts;
+    private static final int[][] directions = {{0, 1}, {1, 1}, {1, 0}, {1, -1}, {0, -1}, {-1, -1}, {-1, 0}, {-1, 1}};
 
     game(int gridSize) {
-        this.neighbors = new int[gridSize][gridSize];
+        grid = new String [gridSize][gridSize];
+        neighborsCounts = new int[grid.length][grid[0].length];
         this.grid = new String[gridSize][gridSize];
         for (int row = 0; row < this.grid.length; row++) {
             for (int cell = 0; cell < this.grid[row].length; cell++) {
@@ -17,50 +18,53 @@ public class game {
             }
             System.out.println();
         }
-        this.checkNeighbors();
     }
 
     void checkNeighbors() {
-        int i = 0;
+        int count = 0;
         for (int row = 0; row < this.grid.length; row++) {
             for (int col = 0; col < this.grid[row].length; col++) {
-                for (int drow = 0; drow < this.directions.length; drow++) {
-                    int x = (this.directions[drow][this.directions[drow].length - this.directions[drow].length]);
-                    int y = (this.directions[drow][this.directions[drow].length - 1]);
-                    x = row + x;
-                    y = col + y;
+                for (int[] direction : directions) {
+                    int y = row + direction[0];
+                    int x = col + direction[1];
                     if (x < 0) {
                         x += this.grid.length;
-                    } else if (x + 1 > this.grid.length) {
-                        x -= x;
+                    } else if  (x >= grid.length) {
+                        x -= grid.length;
                     }
                     if (y < 0) {
                         y += this.grid[row].length;
                     } else if (y + 1 > this.grid[row].length) {
                         y -= y;
                     }
-                    if (this.grid[x][y] == "x|") i++;
+                    if (this.grid[x][y] == "x|") count++;
                 }
-                this.neighbors[row][col] = i;
-                i = 0;
+                neighborsCounts[row][col] = count;
+                count= 0;
             }
         }
-        this.showGrid();
     }
     void showGrid() {
-        for (int row = 0; row < this.grid.length; row++) {
-            for (int cell = 0; cell < this.grid[row].length; cell++) {
-                if (this.grid[row][cell] == " |" && this.neighbors[row][cell] == 3) {
-                    this.grid[row][cell] = "x|";
-                } else if (this.grid[row][cell] == "x|" && this.neighbors[row][cell] > 3) {
-                    this.grid[row][cell] = " |";
-                } else if (this.grid[row][cell] == "x|" && this.neighbors[row][cell] < 2) {
-                    this.grid[row][cell] = " |";
+        int f = 0;
+        while (f !=3){
+                for (int row = 0; row < this.grid.length; row++) {
+                    for (int cell = 0; cell < this.grid[row].length; cell++) {
+                        if (this.grid[row][cell] == " |" && this.neighborsCounts[row][cell] == 3) {
+                            this.grid[row][cell] = "x|";
+                            f++;
+                        } else if (this.grid[row][cell] == "x|" && this.neighborsCounts[row][cell] > 3) {
+                            this.grid[row][cell] = " |";
+                            f++;
+                        } else if (this.grid[row][cell] == "x|" && this.neighborsCounts[row][cell] < 2) {
+                            this.grid[row][cell] = " |";
+                            f++;
+                        }
+                        System.out.print(this.grid[row][cell]);
+                    }
+                    System.out.println();
                 }
-                System.out.print(this.grid[row][cell]);
             }
-            System.out.println();
+            this.checkNeighbors();
         }
-        this.checkNeighbors();
     }
-}
+
