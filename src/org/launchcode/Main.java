d04ea8eac3d2ba9acbccafc39f133b4c180b7139
greg171 <gregory.peters.2016@gmail.com>
2019-01-25 15:36:17 +0000

    Added a while loop so that the calls are not happening within the constructor of game.java

--- src/org/launchcode/Main.java
+++ src/org/launchcode/Main.java
@@ -3,6 +3,10 @@ package org.launchcode;
 public class Main {
 
     public static void main(String[] args) {
-	game g = new game(80);
+	game g = new game(10);
+        while (true) {
+            g.checkNeighbors();
+            g.showGrid();
+        }
     }
 }
