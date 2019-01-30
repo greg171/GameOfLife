6b256c493980dfcd6ddb8d300257ff1e98c59d66
greg171 <gregory.peters.2016@gmail.com>
2019-01-30 17:24:01 +0000

    Refactor, added comments and renamed for easier readability. Now ready to submit to the BBC.

--- src/org/launchcode/Main.java
+++ src/org/launchcode/Main.java
@@ -3,7 +3,7 @@ package org.launchcode;
 public class Main {
 
     public static void main(String[] args) {
-	game g = new game(10);
+	game g = new game(80);
 	int count = 0;
         while (true) {
             g.checkNeighbors();
--- src/org/launchcode/game.java
+++ src/org/launchcode/game.java
@@ -3,21 +3,22 @@ package org.launchcode;
 public class game {
 
 
-    private String[][] grid;
-    private int[][] neighborsCounts;
+    private String[][] gameGrid;
+    private int[][] neighborsCount;
+    //Set as static as directions are constant and will not change.
     private static final int[][] directions = {{0, 1}, {1, 1}, {1, 0}, {1, -1}, {0, -1}, {-1, -1}, {-1, 0}, {-1, 1}};
 
     game(int gridSize) {
         int count = 0;
         //Initialise size of the grid
-        grid = new String[gridSize][gridSize];
-        neighborsCounts = new int[grid.length][grid[0].length];
-        this.grid = new String[gridSize][gridSize];
-
-        for (int row = 0; row < this.grid.length; row++) {
-            for (int cell = 0; cell < this.grid[row].length; cell++) {
-                this.grid[row][cell] = (Math.random() > .9) ? "x|" : " |";
-                System.out.print(this.grid[row][cell]);
+        gameGrid = new String[gridSize][gridSize];
+        neighborsCount = new int[gameGrid.length][gameGrid[0].length];
+        this.gameGrid = new String[gridSize][gridSize];
+        //Fill grid with random cells for game of life
+        for (int row = 0; row < this.gameGrid.length; row++) {
+            for (int cell = 0; cell < this.gameGrid[row].length; cell++) {
+                this.gameGrid[row][cell] = (Math.random() > .9) ? "x|" : " |";
+                System.out.print(this.gameGrid[row][cell]);
 
             }
             System.out.println();
@@ -27,53 +28,62 @@ public class game {
 
     void checkNeighbors() {
         int count = 0;
-        for (int row = 0; row < this.grid.length; row++) {
-            for (int col = 0; col < this.grid[row].length; col++) {
+        for (int row = 0; row < this.gameGrid.length; row++)
+            for (int col = 0; col < this.gameGrid[row].length; col++) {
+                //For each row and column take the direction we want to check
                 for (int[] direction : directions) {
                     int y = row + direction[0];
                     int x = col + direction[1];
+                    //if x less than 0, assign the value of the grid's length
                     if (x < 0) {
-                        x += this.grid.length;
-                    } else if (x >= grid.length) {
-                        x -= grid.length;
+                        x += this.gameGrid.length;
+                    } else if (x >= gameGrid.length) {
+                        //else assign the value minus the length of the grid
+                        x -= gameGrid.length;
                     }
                     if (y < 0) {
-                        y += this.grid[row].length;
-                    } else if (y + 1 > this.grid[row].length) {
+                        //if y less than 0, assign the value of the length of the row
+                        y += this.gameGrid[row].length;
+                    } else if (y + 1 > this.gameGrid[row].length) {
+                        //else assign the value minus the length of the row
                         y -= y;
                     }
-                    if (this.grid[x][y] == "x|") count++;
+                    //if the particular cell has an x in one of the directions then count
+                    if (this.gameGrid[x][y] == "x|") count++;
+
                 }
-                neighborsCounts[row][col] = count;
+                //add the neighbour found and reset the counter for new direction
+                neighborsCount[row][col] = count;
                 count = 0;
             }
-        }
     }
 
     void showGrid() {
-
-        for (int row = 0; row < this.grid.length; row++) {
-            for (int cell = 0; cell < this.grid[row].length; cell++) {
+       //method for grid reprint on new iteration
+        for (int row = 0; row < this.gameGrid.length; row++) {
+            for (int cell = 0; cell < this.gameGrid[row].length; cell++) {
                 //Creation of life scenario
-                if (this.grid[row][cell] == " |" && this.neighborsCounts[row][cell] == 3 ) {
-                    this.grid[row][cell] = "x|";
+                if (this.gameGrid[row][cell] == " |" && this.neighborsCount[row][cell] == 3 ) {
+                    this.gameGrid[row][cell] = "x|";
                     //Overcrowding scenario
-                } else if (this.grid[row][cell] == "x|" && this.neighborsCounts[row][cell] > 3) {
-                    this.grid[row][cell] = " |";
+                } else if (this.gameGrid[row][cell] == "x|" && this.neighborsCount[row][cell] > 3) {
+                    this.gameGrid[row][cell] = " |";
                     // Underpopulation scenario
-                } else if (this.grid[row][cell] == "x|" && this.neighborsCounts[row][cell] < 2) {
-                    this.grid[row][cell] = " |";
+                } else if (this.gameGrid[row][cell] == "x|" && this.neighborsCount[row][cell] < 2) {
+                    this.gameGrid[row][cell] = " |";
                     //No interactions scenario
-                }else if(this.grid[row][cell] == " |" && this.neighborsCounts[row][cell] == 0){
-                    this.grid[row][cell] = " |";
+                }else if(this.gameGrid[row][cell] == " |" && this.neighborsCount[row][cell] == 0){
+                    this.gameGrid[row][cell] = " |";
                     //Survival scenario
-                }else if(this.grid[row][cell] == "x|" && this.neighborsCounts[row][cell] == 2 || this.grid[row][cell] == "x|" && this.neighborsCounts[row][cell] == 3){
-                    this.grid[row][cell] = "x|";
+                }else if(this.gameGrid[row][cell] == "x|" && this.neighborsCount[row][cell] == 2 || this.gameGrid[row][cell] == "x|" && this.neighborsCount[row][cell] == 3){
+                    this.gameGrid[row][cell] = "x|";
                 }
-                System.out.print(this.grid[row][cell]);
+                System.out.print(this.gameGrid[row][cell]);
             }
             System.out.println();
         }
+        System.out.println();
+        //Begin check for neighbours after iteration
         this.checkNeighbors();
     }
 }
